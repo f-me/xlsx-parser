3445bc745c6401cf33b8de03f78a2952fd021c01
Tim <tymmym@gmail.com>
2012-02-07 02:24:17 +0400

    Added date and time conversion for serial values in the 1900 date base system.

--- xlsx-parser.hs
+++ xlsx-parser.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
 
 import Control.Applicative
 import qualified Data.ByteString as B
@@ -6,14 +7,12 @@ import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy as L
 import qualified Data.IntMap as M
 import Data.List
+import Data.Time
 import System.Environment (getArgs)
 
 import Codec.Archive.Zip
 import Data.ByteString.Nums.Careless.Int
 import Text.HTML.TagSoup
-
-import Data.Array.Unboxed
-import Foreign.StablePtr
 
 
 data WorkBook = WorkBook
@@ -27,10 +26,14 @@ data WorkSheet =  WorkSheet
 
 data Cell = Cell
     { cellName  :: !B.ByteString
-    , cellValue :: Either Int B.ByteString
+    , cellValue :: Value
     -- style?
     } deriving Show
 
+data Value = Shared Int
+           | Value B.ByteString
+           | Time UTCTime
+             deriving Show
 
 parseXslx :: L.ByteString -> WorkBook
 parseXslx xsl = WorkBook {sharedStrings = sStrs, workSheets = map work sheets}
@@ -72,38 +75,46 @@ parseSharedStrings xml =
     [B.concat $ L.toChunks x | TagText x <- parseTags xml]
 
 
-type Arr = UArray Int (StablePtr B.ByteString)
-
-mkArr :: Int -> [B.ByteString] -> IO Arr
-mkArr sz xs = listArray (0,sz-1) <$> mapM newStablePtr xs
-
-freeArr :: Arr -> IO ()
-freeArr = mapM_ freeStablePtr . elems
-
-arrGet :: Arr -> Int -> IO B.ByteString
-arrGet a i = deRefStablePtr $ a ! i
-
-
 parseWorksheet :: L.ByteString -> WorkSheet
 parseWorksheet xml = WorkSheet $ loop [] $ parseTags xml
   where
     loop row (TagOpen "c" attrs : TagOpen "v" _ : TagText val : rest) =
         loop (foldl' updCell undefCell attrs : row) rest
       where
-        undefCell = Cell {cellName = "undefined", cellValue = Right $ B.concat $ L.toChunks val}
+        undefCell = Cell {cellName = "undefined", cellValue = Value . B.concat $ L.toChunks val}
         updCell c ("r", r) = c {cellName = B.concat $ L.toChunks r}
-        updCell c ("t","s") = c {cellValue = Left $ int val}
+        updCell c ("t","s") = c {cellValue = Shared $ int val}
+        updCell c ("s","1") = c {cellValue = Time . getTime . C.unpack . B.concat $ L.toChunks val}
         updCell c _ = c
     loop row (TagClose "row" : rest) =  reverse row : loop [] rest
     loop row (_:rest) = loop row rest
     loop _ [] = []
 
+-- Parse date and time in the 1900 date base system.
+-- ECMA-376, 3d edition:
+-- "In the 1900 date base system, the lower limit is January 1, -9999
+-- 00:00:00, which has serial value - 4346018. The upper-limit is
+-- December 31, 9999, 23:59:59, which has serial value
+-- 2,958,465.9999884.  The base date for this date base system is
+-- December 30, 1899, which has a serial value of 0."
+getTime :: String -> UTCTime
+getTime s = UTCTime day diff
+  where
+    val  = read s
+
+    days = truncate val
+    day  = addDays days $ fromGregorian 1899 12 30
+
+    time = val - (fromIntegral days)
+    sec  = round $ time * 86400
+    diff = secondsToDiffTime sec
 
 main = do
     f:_ <- getArgs
     wb <- parseXslx <$> L.readFile f
     print $ M.size $ sharedStrings wb
     print $ foldl' (+) 0 $ map B.length $ M.elems $ sharedStrings wb
+--    print $ foldl' (+) 0 $ map length $ concatMap cells $ workSheets wb
 
 test f (cols, start) = do
     xlsx <- L.readFile f
