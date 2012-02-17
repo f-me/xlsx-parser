50f6e9794046fe934f1a8caebd3de0cff8604f69
Max Taldykin <jorpic@gmail.com>
2012-02-17 20:51:47 +0400

    parsing sheets into raw cells

--- src/Data/Xlsx/Parser.hs
+++ src/Data/Xlsx/Parser.hs
@@ -9,6 +9,7 @@ import Control.Monad.IO.Class
 import Data.List
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Read as T
 import qualified Data.ByteString.Lazy as L
 
 import qualified Data.IntMap as M
@@ -16,11 +17,10 @@ import qualified Data.IntMap as M
 import qualified Codec.Archive.Zip as Zip
 
 import Data.Conduit
+import Data.XML.Types
 import qualified Data.Conduit.List as CL
 import qualified Text.XML.Stream.Parse as Xml
 
-
-type Column = String
 
 data Xlsx
   = Xlsx
@@ -34,7 +34,7 @@ data Columns
   | Columns [String]
 
 data Cell
-  = Cell {id :: Text, style::Int, value::Text}
+  = Cell {cellId :: Text, style :: Int, value :: Maybe Text}
   deriving Show
 
 
@@ -61,11 +61,45 @@ xlsx fname = do
 parseSheet x@(Xlsx{..}) sheetId columns
   | sheetId < 0 || sheetId >= length sheets
     = error "parseSheet: Invalid sheetId"
-  | otherwise = xmlSource x (sheets !! sheetId)
+  | otherwise
+    = case xmlSource x (sheets !! sheetId) of
+      Nothing -> error "An impossible happened"
+      Just xml -> xml $= mkXmlCond getCell $$ CL.consume
   where
     sheets = sort
       $ filter (isPrefixOf "xl/worksheets")
       $ Zip.filesInArchive archive
+
+
+getCell = Xml.tagName (n"c") cAttrs cParser
+  where
+    cAttrs = do
+      cellId  <- Xml.requireAttr  "r"
+      style   <- Xml.requireAttr  "s"
+      sharing <- Xml.optionalAttr "t"
+      Xml.ignoreAttrs
+      return (cellId,style,sharing)
+
+    cParser a@(cellId,style,sharing) = do
+      val <- case sharing of
+          Just "inlineStr" -> tagSeq ["is", "t"]
+          Just "s" -> tagSeq ["v"] >> return (Just "shared str")
+          Nothing  -> tagSeq ["v"]
+      return $ Cell cellId (int style) val
+
+n x = Name
+  {nameLocalName = x
+  ,nameNamespace = Just "http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+  ,namePrefix = Nothing}
+
+tagSeq (x:xs)
+  = Xml.tagNoAttr (n x)
+  $ foldr (\x -> Xml.force "" . Xml.tagNoAttr (n x)) Xml.content xs
+
+
+-- | Parse decimal from Text
+int = either error fst . T.decimal 
+
 
 xmlSource (Xlsx{..}) fname
   =   Xml.parseLBS Xml.def
@@ -87,6 +121,8 @@ parseSharedStrings x
 xmlToText xml = xml $= (mkXmlCond Xml.contentMaybe) $$ CL.consume
 
 -- | Create conduit from xml sink
+-- Resulting conduit filters nodes that `f` can consume and skips everything
+-- else.
 mkXmlCond f = sequenceSink () $ const
   $ CL.peek >>= maybe          -- try get current event form the stream
     (return Stop)              -- stop if stream is empty
